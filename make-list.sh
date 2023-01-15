dde4b4b78f356628f658a5d8f4259b3a55cda85a
vonhyou <i@designart.eu.org>
2023-01-15 02:13:19 -0400

    fix fatal error

--- make-list.sh
+++ make-list.sh
@@ -7,7 +7,7 @@ while read -r line; do
     source=$(echo $line | awk '{print $1}')
     destination=$(echo $line | awk '{print $2}')
     code=$(echo $line | awk '{print $3}')
-    redirectList="$redirectList<li><a href=\"$destination\">$source</a></li>"
+    redirectList="$redirectList<li><a href=\"$source\">$source</a></li>"
 done < "$file"
 
 html="<ul>$redirectList</ul>"
