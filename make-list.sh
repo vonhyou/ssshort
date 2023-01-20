f617d2182878f06298664cf9ad37af0f4c50ac9c
过期薯条 <i@designart.eu.org>
2023-01-19 21:19:38 -0400

    Remove unused variables

--- make-list.sh
+++ make-list.sh
@@ -5,8 +5,6 @@ redirectList=""
 
 while read -r line; do
     source=$(echo $line | awk '{print $1}')
-    destination=$(echo $line | awk '{print $2}')
-    code=$(echo $line | awk '{print $3}')
     redirectList="$redirectList<li><a href=\"$source\">$source</a></li>"
 done < "$file"
 
