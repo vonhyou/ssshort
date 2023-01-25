5014a1fa8a5e255a49b81d7bc139a7504de8e9db
vonhyou <i@designart.eu.org>
2023-01-24 21:40:44 -0400

    reformat

--- make-list.sh
+++ make-list.sh
@@ -4,10 +4,10 @@ file="./_redirects"
 redirectList=""
 
 while read -r line; do
-    source=$(echo $line | awk '{print $1}')
-    destination=$(echo $line | awk '{print $2}')
-    code=$(echo $line | awk '{print $3}')
-    redirectList="$redirectList<li><a href=\"$source\">$source</a></li>"
+  source=$(echo $line | awk '{print $1}')
+  destination=$(echo $line | awk '{print $2}')
+  code=$(echo $line | awk '{print $3}')
+  redirectList="$redirectList<li><a href=\"$source\">$source</a></li>"
 done < "$file"
 
 html="<ul>$redirectList</ul>"
